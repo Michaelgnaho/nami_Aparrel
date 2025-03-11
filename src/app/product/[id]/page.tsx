c6b857ee01b878dbd9167867953634581347a09b
Michaelgnaho <gnahomiael@gmail.com>
2025-03-11 21:27:23 +0100

    update

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,11 +1,13 @@
 // src/app/product/[id]/page.tsx
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck - Disable TypeScript checking for this file
+// src/app/product/[id]/page.tsx
 import React from "react";
 import products from "@/app/lib/product";
 import ProductDisplay from "./product-display";
 
-export default function ProductPage({ params }) {
+export default async function ProductPage({ params }: { params: { id: string } }) {
+  // With Next.js App Router, we need to properly handle params
   const productId = parseInt(params.id);
   const product = products.find((p) => p.id === productId);
   
